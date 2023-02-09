d7a28dae73b5616354ccceaecfc87e70dd858e07
Vinicius <vinicius.cej@gmail.com>
2023-02-09 12:03:02 -0300

    first commit to my personal portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css'
+import Title from './components/title';
+
+function App() {
+  return (
+    <div className="App">      
+      <Title />
+      <p className='subtitle'>
+        Junior Software Developer
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Title() {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", width: 150 }}>
+      <div style={{ textAlign: "left" }}>Vinicius</div>
+      <div style={{ textAlign: "center" }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dal</div>
+      <div style={{ textAlign: "right" }}>Bello</div>
+    </div>
+  )
+}
+
+export default Title;
